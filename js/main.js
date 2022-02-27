f8e39425dcb653dbdae94a6897691865db5d2526
KatieMarie <kguite@gmail.com>
2022-02-27 14:21:21 -0800

    first commit

---
+++ js/main.js
@@ -0,0 +1,80 @@
+const finn = document.querySelector('#finn')
+const jake = document.querySelector('#jake')
+const bmo = document.querySelector('#bmo')
+const iceKing = document.querySelector('#iceKing')
+const princessBubblegum = document.querySelector('#princessBubblegum')
+const jamesBaxter = document.querySelector('#jamesBaxter')
+const all = document.querySelector('#all')
+
+document.querySelector('#finnDisplay').addEventListener('click', finnDisplay)
+document.querySelector('#jakeDisplay').addEventListener('click', jakeDisplay)
+document.querySelector('#bmoDisplay').addEventListener('click', bmoDisplay)
+document.querySelector('#iceKingDisplay').addEventListener('click', iceKingDisplay)
+document.querySelector('#princessBubblegumDisplay').addEventListener('click', princessBubblegumDisplay)
+document.querySelector('#jamesBaxterDisplay').addEventListener('click', jamesBaxterDisplay)
+document.querySelector('#allDisplay').addEventListener('click', allDisplay)
+
+
+
+function finnDisplay(){
+	finn.classList.toggle('hidden')
+	// jake.classList.add('hidden')
+	// bmo.classList.add('hidden')
+	// iceKing.classList.add('hidden')
+	// princessBubblegum.classList.add('hidden')
+	// jamesBaxter.classList.add('hidden')
+}
+
+function jakeDisplay(){
+	// finn.classList.add('hidden')
+	jake.classList.toggle('hidden')
+	// bmo.classList.add('hidden')
+	// iceKing.classList.add('hidden')
+	// princessBubblegum.classList.add('hidden')
+	// jamesBaxter.classList.add('hidden')
+}
+
+function bmoDisplay(){
+	// finn.classList.add('hidden')
+	// jake.classList.add('hidden')
+	bmo.classList.toggle('hidden')
+	// iceKing.classList.add('hidden')
+	// princessBubblegum.classList.add('hidden')
+	// jamesBaxter.classList.add('hidden')
+}
+
+function iceKingDisplay(){
+	// finn.classList.add('hidden')
+	// jake.classList.add('hidden')
+	// bmo.classList.add('hidden')
+	iceKing.classList.toggle('hidden')
+	// princessBubblegum.classList.add('hidden')
+	// jamesBaxter.classList.add('hidden')
+}
+
+function princessBubblegumDisplay(){
+	// finn.classList.add('hidden')
+	// jake.classList.add('hidden')
+	// bmo.classList.add('hidden')
+	// iceKing.classList.add('hidden')
+	princessBubblegum.classList.toggle('hidden')
+	// jamesBaxter.classList.add('hidden')
+}
+
+function jamesBaxterDisplay(){
+	// finn.classList.add('hidden')
+	// jake.classList.add('hidden')
+	// bmo.classList.add('hidden')
+	// iceKing.classList.add('hidden')
+	// princessBubblegum.classList.add('hidden')
+	jamesBaxter.classList.toggle('hidden')
+}
+
+function allDisplay(){
+	finn.classList.add('visible')
+	jake.classList.add('visible')
+	bmo.classList.add('visible')
+	iceKing.classList.add('visible')
+	princessBubblegum.classList.add('visible')
+	jamesBaxter.classList.add('visible')
+}
